c9da21e7e11d8371d23742ed75e85a553efd7410
phuchoang <phuchoang.vnde@gmail.com>
2024-04-24 17:44:50 +0200

    page-render routers + api-router for login and register

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
 
+import { 
+    handleLogin, 
+    handleRegister 
+} from '../controllers/apiController';
+
+
 const apiRouter = Router();
 
 // TODO: routers - logic api 
+apiRouter.post('/login', handleLogin);
+apiRouter.post('/register', handleRegister);
+
 
 export default apiRouter;
