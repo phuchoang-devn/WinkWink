bd9e0385dc91d9e5f933479ec94d8e3244851f7f
vlad_kys <Vladyslav.Kyselov@Student.HTW-Berlin.de>
2024-04-24 16:59:08 +0200

    Routers

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -4,4 +4,29 @@ const apiRouter = Router();
 
 // TODO: routers - logic api 
 
+apiRouter.post('/register', (req, res) => {
+  //logic
+})
+apiRouter.get('/profiles', (req, res) => {
+    //logic
+  })
+  apiRouter.post('/action', (req, res) => {
+    //logic
+  })
+  apiRouter.post('/chat', (req, res) => {
+    //logic
+  })
+  apiRouter.delete('/chat', (req, res) => {
+    //logic
+  })
+  apiRouter.put('/settings', (req, res) => {
+    //logic
+  })
+  apiRouter.put('/password', (req, res) => {
+    //logic
+  })
+  apiRouter.put('/prefrence', (req, res) => {
+    //logic
+  })
+
 export default apiRouter;
