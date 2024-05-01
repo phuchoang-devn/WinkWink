9c7b6cf3e095f36948bf531814183bfc63775dda
phuchoang <phuchoang.vnde@gmail.com>
2024-05-01 20:03:32 +0200

    fakeDB

---
+++ client/src/fakeDB.js
@@ -0,0 +1,388 @@
+export const accounts = [
+    {
+        "id": "e37c45c0-1a6e-4f12-bb3d-9bcf01c04e3a",
+        "email": "user1@example.com",
+        "password": "password1",
+        "userId": "b87fdd1e-e20d-4c6f-9e8c-501d23f92a39"
+    },
+    {
+        "id": "f9a2a859-6a64-4cf1-87d5-18d5cb47a1e2",
+        "email": "user2@example.com",
+        "password": "password2",
+        "userId": "65e11a07-7b2a-47d5-97b7-c1f3643d654e"
+    },
+    {
+        "id": "87e61877-cd79-43e1-9c36-05d5d6279f5d",
+        "email": "user3@example.com",
+        "password": "password3",
+        "userId": "a9f1a85c-2d22-4c43-baf3-8bfa61fb83c6"
+    },
+    {
+        "id": "d4c58a2f-7fae-47fc-94f2-f3accc6eb17e",
+        "email": "user4@example.com",
+        "password": "password4",
+        "userId": "893857d1-2ec3-47ae-9d42-f58231e23947"
+    },
+    {
+        "id": "c18e02fc-b23a-44a4-ba1d-af8e96c42f1b",
+        "email": "user5@example.com",
+        "password": "password5",
+        "userId": "cd1e80d0-413b-4fae-92d1-104774b99a6c"
+    },
+    {
+        "id": "fe5fa8e1-8f17-4cb3-80f3-8c3c2141aef7",
+        "email": "user6@example.com",
+        "password": "password6",
+        "userId": "f1fd9a45-0d8e-487c-bc7c-4f78602ad05a"
+    },
+    {
+        "id": "d3973433-14e1-453e-81c2-109d18b69d46",
+        "email": "user7@example.com",
+        "password": "password7",
+        "userId": "e421b90e-0c64-4daa-b02b-66c5bbfd4c8b"
+    },
+    {
+        "id": "6b89a2d5-1a5e-45e6-b4c6-7348646a14c6",
+        "email": "user8@example.com",
+        "password": "password8",
+        "userId": "ff4358b7-9a32-44d0-ae3f-7d547ab2f28c"
+    },
+    {
+        "id": "f321f2c1-06d6-4868-a8d0-1e6cf6db9a39",
+        "email": "user9@example.com",
+        "password": "password9",
+        "userId": "9a5e872f-9a4b-4a86-b8b8-0a0e84ae8c3a"
+    },
+    {
+        "id": "1fe485b3-77e6-42b6-8f9b-0eaa1e7ee7d6",
+        "email": "user10@example.com",
+        "password": "password10",
+        "userId": "d91030d8-efb7-4d7e-8967-96cb94f86da2"
+    }
+];
+
+export const users = [
+    {
+        "id": "e37c45c0-1a6e-4f12-bb3d-9bcf01c04e3a",
+        "name": {
+            "first": "John",
+            "last": "Doe"
+        },
+        "profileImage": "user1.jpg",
+        "age": 28,
+        "sex": "male",
+        "city": "NYC",
+        "interests": "Reading, hiking, photography",
+        "language": ["eng"],
+        "accountId": "b87fdd1e-e20d-4c6f-9e8c-501d23f92a39"
+    },
+    {
+        "id": "f9a2a859-6a64-4cf1-87d5-18d5cb47a1e2",
+        "name": {
+            "first": "Jane",
+            "last": "Smith"
+        },
+        "profileImage": "user2.jpg",
+        "age": 35,
+        "sex": "female",
+        "city": "LAX",
+        "interests": "Traveling, cooking, yoga",
+        "language": ["eng"],
+        "accountId": "65e11a07-7b2a-47d5-97b7-c1f3643d654e"
+    },
+    {
+        "id": "87e61877-cd79-43e1-9c36-05d5d6279f5d",
+        "name": {
+            "first": "David",
+            "last": "Johnson"
+        },
+        "profileImage": "user3.jpg",
+        "age": 24,
+        "sex": "male",
+        "city": "SFO",
+        "interests": "Gaming, programming, music",
+        "language": ["eng"],
+        "accountId": "a9f1a85c-2d22-4c43-baf3-8bfa61fb83c6"
+    },
+    {
+        "id": "d4c58a2f-7fae-47fc-94f2-f3accc6eb17e",
+        "name": {
+            "first": "Sarah",
+            "last": "Williams"
+        },
+        "profileImage": "user4.jpg",
+        "age": 31,
+        "sex": "female",
+        "city": "SEA",
+        "interests": "Painting, cycling, movies",
+        "language": ["eng"],
+        "accountId": "893857d1-2ec3-47ae-9d42-f58231e23947"
+    },
+    {
+        "id": "c18e02fc-b23a-44a4-ba1d-af8e96c42f1b",
+        "name": {
+            "first": "Michael",
+            "last": "Brown"
+        },
+        "profileImage": "user5.jpg",
+        "age": 29,
+        "sex": "male",
+        "city": "CHI",
+        "interests": "Sports, movies, traveling",
+        "language": ["eng"],
+        "accountId": "cd1e80d0-413b-4fae-92d1-104774b99a6c"
+    },
+    {
+        "id": "fe5fa8e1-8f17-4cb3-80f3-8c3c2141aef7",
+        "name": {
+            "first": "Emily",
+            "last": "Jones"
+        },
+        "profileImage": "user6.jpg",
+        "age": 26,
+        "sex": "female",
+        "city": "ATL",
+        "interests": "Reading, hiking, movies",
+        "language": ["eng"],
+        "accountId": "f1fd9a45-0d8e-487c-bc7c-4f78602ad05a"
+    },
+    {
+        "id": "d3973433-14e1-453e-81c2-109d18b69d46",
+        "name": {
+            "first": "Matthew",
+            "last": "Davis"
+        },
+        "profileImage": "user7.jpg",
+        "age": 33,
+        "sex": "male",
+        "city": "DAL",
+        "interests": "Cooking, sports, music",
+        "language": ["eng"],
+        "accountId": "e421b90e-0c64-4daa-b02b-66c5bbfd4c8b"
+    },
+    {
+        "id": "6b89a2d5-1a5e-45e6-b4c6-7348646a14c6",
+        "name": {
+            "first": "Olivia",
+            "last": "Wilson"
+        },
+        "profileImage": "user8.jpg",
+        "age": 30,
+        "sex": "female",
+        "city": "MIA",
+        "interests": "Yoga, reading, painting",
+        "language": ["eng"],
+        "accountId": "ff4358b7-9a32-44d0-ae3f-7d547ab2f28c"
+    },
+    {
+        "id": "f321f2c1-06d6-4868-a8d0-1e6cf6db9a39",
+        "name": {
+            "first": "James",
+            "last": "Martinez"
+        },
+        "profileImage": "user9.jpg",
+        "age": 27,
+        "sex": "male",
+        "city": "DEN",
+        "interests": "Photography, traveling, sports",
+        "language": ["eng"],
+        "accountId": "9a5e872f-9a4b-4a86-b8b8-0a0e84ae8c3a"
+    },
+    {
+        "id": "1fe485b3-77e6-42b6-8f9b-0eaa1e7ee7d6",
+        "name": {
+            "first": "Sophia",
+            "last": "Rodriguez"
+        },
+        "profileImage": "user10.jpg",
+        "age": 32,
+        "sex": "female",
+        "city": "HOU",
+        "interests": "Movies, cooking, gardening",
+        "language": ["eng"],
+        "accountId": "d91030d8-efb7-4d7e-8967-96cb94f86da2"
+    }
+];
+
+export const chats = [
+    {
+        "chatId": "c81852e3-98d8-4a09-98cb-6d3bb28f2757",
+        "sender": "e37c45c0-1a6e-4f12-bb3d-9bcf01c04e3a",
+        "receiver": "f9a2a859-6a64-4cf1-87d5-18d5cb47a1e2",
+        "content": "Hey Jane, how are you?",
+        "timestamp": {
+            "$date": "2024-05-01T10:15:00Z"
+        }
+    },
+    {
+        "chatId": "ddc7a8b0-8f40-42f3-ba9c-3d69a187c05b",
+        "sender": "f9a2a859-6a64-4cf1-87d5-18d5cb47a1e2",
+        "receiver": "e37c45c0-1a6e-4f12-bb3d-9bcf01c04e3a",
+        "content": "Hi John! I'm doing well, thanks. How about you?",
+        "timestamp": {
+            "$date": "2024-05-01T10:20:00Z"
+        }
+    },
+    {
+        "chatId": "1ec18c6d-510b-442d-8a53-6e78d1b59c24",
+        "sender": "87e61877-cd79-43e1-9c36-05d5d6279f5d",
+        "receiver": "d4c58a2f-7fae-47fc-94f2-f3accc6eb17e",
+        "content": "Hey Sarah, have you seen the latest movie?",
+        "timestamp": {
+            "$date": "2024-05-01T11:00:00Z"
+        }
+    },
+    {
+        "chatId": "12cf8cb7-1d8e-4a68-b2df-157d7de1d9c2",
+        "sender": "d4c58a2f-7fae-47fc-94f2-f3accc6eb17e",
+        "receiver": "87e61877-cd79-43e1-9c36-05d5d6279f5d",
+        "content": "No, not yet! I heard it's great though.",
+        "timestamp": {
+            "$date": "2024-05-01T11:05:00Z"
+        }
+    },
+    {
+        "chatId": "8d20d72e-e7e7-4a41-a21a-6a98fe90f4e6",
+        "sender": "c18e02fc-b23a-44a4-ba1d-af8e96c42f1b",
+        "receiver": "fe5fa8e1-8f17-4cb3-80f3-8c3c2141aef7",
+        "content": "Hi Emily, are you up for a hike this weekend?",
+        "timestamp": {
+            "$date": "2024-05-01T12:30:00Z"
+        }
+    },
+    {
+        "chatId": "5a20df7b-ba57-4b39-9a62-1c2d2b47e8fc",
+        "sender": "fe5fa8e1-8f17-4cb3-80f3-8c3c2141aef7",
+        "receiver": "c18e02fc-b23a-44a4-ba1d-af8e96c42f1b",
+        "content": "Sure Michael, sounds great! Count me in.",
+        "timestamp": {
+            "$date": "2024-05-01T12:35:00Z"
+        }
+    },
+    {
+        "chatId": "a5f8d8d8-727b-4f25-b83f-5cb26c3f34e5",
+        "sender": "d3973433-14e1-453e-81c2-109d18b69d46",
+        "receiver": "6b89a2d5-1a5e-45e6-b4c6-7348646a14c6",
+        "content": "Hi Olivia, how was your weekend?",
+        "timestamp": {
+            "$date": "2024-05-01T13:00:00Z"
+        }
+    },
+    {
+        "chatId": "34b8e400-7151-4f14-8ba0-8d03aeb2b9bc",
+        "sender": "6b89a2d5-1a5e-45e6-b4c6-7348646a14c6",
+        "receiver": "d3973433-14e1-453e-81c2-109d18b69d46",
+        "content": "It was great, thanks! How about yours?",
+        "timestamp": {
+            "$date": "2024-05-01T13:05:00Z"
+        }
+    },
+    {
+        "chatId": "7f033640-88df-4f2b-b3ae-385ee8e87eb3",
+        "sender": "f321f2c1-06d6-4868-a8d0-1e6cf6db9a39",
+        "receiver": "1fe485b3-77e6-42b6-8f9b-0eaa1e7ee7d6",
+        "content": "Hey Sophia, let's plan a trip for next month!",
+        "timestamp": {
+            "$date": "2024-05-01T14:00:00Z"
+        }
+    },
+    {
+        "chatId": "3d1b208a-0717-4aa4-8649-74972f95fa5d",
+        "sender": "1fe485b3-77e6-42b6-8f9b-0eaa1e7ee7d6",
+        "receiver": "f321f2c1-06d6-4868-a8d0-1e6cf6db9a39",
+        "content": "Sounds awesome, James! I'm in.",
+        "timestamp": {
+            "$date": "2024-05-01T14:05:00Z"
+        }
+    },
+    {
+        "chatId": "7caaa69a-2a07-4ab7-b0a0-c4936f49a5a3",
+        "sender": "e37c45c0-1a6e-4f12-bb3d-9bcf01c04e3a",
+        "receiver": "87e61877-cd79-43e1-9c36-05d5d6279f5d",
+        "content": "David, did you finish that project?",
+        "timestamp": {
+            "$date": "2024-05-01T15:00:00Z"
+        }
+    },
+    {
+        "chatId": "a5f6822f-39e6-4e0f-b9a6-42f8b7e8c97e",
+        "sender": "87e61877-cd79-43e1-9c36-05d5d6279f5d",
+        "receiver": "e37c45c0-1a6e-4f12-bb3d-9bcf01c04e3a",
+        "content": "Not yet, but almost done!",
+        "timestamp": {
+            "$date": "2024-05-01T15:05:00Z"
+        }
+    },
+    {
+        "chatId": "3ab20f0e-6817-4371-9269-d9f5c9a82c0b",
+        "sender": "c18e02fc-b23a-44a4-ba1d-af8e96c42f1b",
+        "receiver": "d4c58a2f-7fae-47fc-94f2-f3accc6eb17e",
+        "content": "Sarah, let's grab lunch tomorrow.",
+        "timestamp": {
+            "$date": "2024-05-01T16:00:00Z"
+        }
+    },
+    {
+        "chatId": "cdcd55c3-b57a-4b0a-9e17-cd72a4430636",
+        "sender": "d4c58a2f-7fae-47fc-94f2-f3accc6eb17e",
+        "receiver": "c18e02fc-b23a-44a4-ba1d-af8e96c42f1b",
+        "content": "Sure Michael, I'm free around noon.",
+        "timestamp": {
+            "$date": "2024-05-01T16:05:00Z"
+        }
+    },
+    {
+        "chatId": "42f268a6-c3ab-47c6-b46e-2d3a47329298",
+        "sender": "fe5fa8e1-8f17-4cb3-80f3-8c3c2141aef7",
+        "receiver": "6b89a2d5-1a5e-45e6-b4c6-7348646a14c6",
+        "content": "Olivia, let's plan a movie night.",
+        "timestamp": {
+            "$date": "2024-05-01T17:00:00Z"
+        }
+    },
+    {
+        "chatId": "c06fc5f0-4c3c-44d3-9eb2-7c7505643f72",
+        "sender": "6b89a2d5-1a5e-45e6-b4c6-7348646a14c6",
+        "receiver": "fe5fa8e1-8f17-4cb3-80f3-8c3c2141aef7",
+        "content": "Sounds good, Emily! I'll bring popcorn.",
+        "timestamp": {
+            "$date": "2024-05-01T17:05:00Z"
+        }
+    },
+    {
+        "chatId": "b7bc78ed-d0d6-48b1-bf1d-b641ad7e7f3d",
+        "sender": "d3973433-14e1-453e-81c2-109d18b69d46",
+        "receiver": "f321f2c1-06d6-4868-a8d0-1e6cf6db9a39",
+        "content": "James, are you joining us for dinner tonight?",
+        "timestamp": {
+            "$date": "2024-05-01T18:00:00Z"
+        }
+    },
+    {
+        "chatId": "dc8b2647-3e9a-4cf9-aa40-cc97982b8e8c",
+        "sender": "f321f2c1-06d6-4868-a8d0-1e6cf6db9a39",
+        "receiver": "d3973433-14e1-453e-81c2-109d18b69d46",
+        "content": "Sure Matthew, what time?",
+        "timestamp": {
+            "$date": "2024-05-01T18:05:00Z"
+        }
+    },
+    {
+        "chatId": "e7ac2318-b263-41e1-bd17-39e130788c30",
+        "sender": "1fe485b3-77e6-42b6-8f9b-0eaa1e7ee7d6",
+        "receiver": "f9a2a859-6a64-4cf1-87d5-18d5cb47a1e2",
+        "content": "Jane, let's catch up over coffee.",
+        "timestamp": {
+            "$date": "2024-05-01T19:00:00Z"
+        }
+    },
+    {
+        "chatId": "92d63b5b-3191-41ff-9ff1-5289ba0c48bb",
+        "sender": "f9a2a859-6a64-4cf1-87d5-18d5cb47a1e2",
+        "receiver": "1fe485b3-77e6-42b6-8f9b-0eaa1e7ee7d6",
+        "content": "Sure John, let's meet at the usual place.",
+        "timestamp": {
+            "$date": "2024-05-01T19:05:00Z"
+        }
+    }
+];
