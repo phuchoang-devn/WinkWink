9f111698de0961f7b71c1d998f32558f5aa7a495
margo250494@gmail.com <Margo250494@github.com>
2024-07-04 13:13:20 +0200

    404 Error

--- client/src/components/nopage.jsx
+++ client/src/components/nopage.jsx
@@ -1,7 +1,15 @@
+import '../index.scss'
+import { useNavigate } from 'react-router-dom';
+
 const NoPage = (props) => {
+    const navigate = useNavigate();
     return(
-        <div>
-            404 Page not found
+        <div className='not-found'>
+            <h1>404 Error</h1>
+            <h3>Your wink didn't reach this page.
+                But don't worry, many others are waiting for your wink!
+                Back to the homepage!</h3>
+            <button onClick={() => navigate('/')}>Back Home</button>
         </div>
     );
 }
